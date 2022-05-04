e0cd007fa06cd8ba11b3ed552cfd40c1419371b6
Kenichi Fukunaga <dmc.system.service@gmail.com>
2022-05-04 19:48:57 +0900

    Create provision.groovy

---
+++ provision.groovy
@@ -0,0 +1,25 @@
+import hudson.model.Label
+import hudson.model.Node
+import hudson.model.labels.LabelAtom
+import hudson.plugins.ec2.AmazonEC2Cloud
+import hudson.plugins.ec2.EC2AbstractSlave
+import hudson.plugins.ec2.SlaveTemplate
+import jenkins.model.Jenkins
+
+// provision EC2 instances for agent label
+label = "cppcheck"
+agent = new LabelAtom(label)
+
+Jenkins.instance.clouds.findAll { cloud ->    
+    (cloud in AmazonEC2Cloud) && cloud.canProvision(agent) 
+}.each { cloud ->
+
+    template = cloud.getTemplate(agent)
+    List<Node> nodes = template.provision(1, EnumSet.of(SlaveTemplate.ProvisionOptions.FORCE_CREATE))
+    nodes.each { node ->
+        Jenkins.instance.addNode(node)
+    }
+    println "Provisioned ${label} as new agent."
+} ?: println('No agents provisioned.')
+
+null
